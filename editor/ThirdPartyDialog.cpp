3f8ede693d19049abc4ceaaef8b6043dc31d4498
Nikolay Zapolnov <zapolnov@gmail.com>
2019-09-01 13:39:28 +0300

    Added forgotten license notice for Lua.

--- editor/ThirdPartyDialog.cpp
+++ editor/ThirdPartyDialog.cpp
@@ -68,6 +68,31 @@ ThirdPartyDialog::ThirdPartyDialog(QWidget* parent)
         "TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE<br>"
         "OR PERFORMANCE OF THIS SOFTWARE.</p>"
 
+        "<hr>"
+
+        "<p>This program uses Lua:<br>"
+        "<a href=\"https://www.lua.org/\">https://www.lua.org/</a><br>"
+        "Copyright &copy; 1994-2018 Lua.org, PUC-Rio.<br>"
+        "<br>"
+        "Permission is hereby granted, free of charge, to any person obtaining<br>"
+        "a copy of this software and associated documentation files (the<br>"
+        "\"Software\"), to deal in the Software without restriction, including<br>"
+        "without limitation the rights to use, copy, modify, merge, publish,<br>"
+        "distribute, sublicense, and/or sell copies of the Software, and to<br>"
+        "permit persons to whom the Software is furnished to do so, subject to<br>"
+        "the following conditions:<br>"
+        "<br>"
+        "The above copyright notice and this permission notice shall be<br>"
+        "included in all copies or substantial portions of the Software.<br>"
+        "<br>"
+        "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,<br>"
+        "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF<br>"
+        "MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.<br>"
+        "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY<br>"
+        "CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,<br>"
+        "TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE<br>"
+        "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>"
+
         "</body></html>"
         ));
 }
